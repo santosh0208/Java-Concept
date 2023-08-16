7f017c42c10c5918ef9f335226698929faa71a08
santosh0208 <santoshalamuru0@msitprogram.net>
2023-08-16 14:27:21 +0530

    Java Concepts

---
+++ Collections/Interface/Client.java
@@ -0,0 +1,30 @@
+package Collections.Interface;
+
+import java.util.Scanner;
+
+interface Client{
+    void input();
+    void output();
+}
+class santosh implements Client{
+    String name;
+    double salary;
+    public void input(){
+        Scanner r = new Scanner(System.in);
+        System.out.println("Enter Username :");
+        name = r.nextLine();
+
+        System.out.println("Enter Salary :");
+        salary = r.nextDouble();
+    }
+    public void output(){
+        System.out.println(name + " ");
+        System.out.println(salary +" ");
+    }
+    public static void main(String[] args){
+        Client c = new santosh();
+        c.input();
+        c.output();
+    }
+}
+
